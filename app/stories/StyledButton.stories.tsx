17d9e36f76caddc95d0f1f4cf38b496c4cbf560c
masakina0518 <masakina0518@gmail.com>
2022-10-31 14:10:06 +0900

    Controllerタブを利用したprops制御

--- app/stories/StyledButton.stories.tsx
+++ app/stories/StyledButton.stories.tsx
@@ -1,44 +1,69 @@
 import { useState } from 'react'
-import { ComponentMeta } from '@storybook/react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 import React from 'react'
 import { StyledButton } from '../components/StyledButton'
-import { action } from '@storybook/addon-actions'
 
 export default {
     title: 'StyledButton',
     component: StyledButton,
+    argTypes: {
+        variant: {
+            control: {type: 'radio'},
+            options: ['primary', 'success', 'transparent'],
+        },
+        children: {
+            control: {type: 'text'},
+        },
+    }
 } as ComponentMeta<typeof StyledButton>
 
-const incrementAction = action('increment')
+const Template: ComponentStory<typeof StyledButton> = (args) => <StyledButton {...args} />
 
-export const Primary = (props) => {
+export const TemplateTest = Template.bind({})
 
-    const [count, setCount] = useState(0)
-    const onClick = (e: React.MouseEvent) => {
-        incrementAction(e, count)
-        setCount((c) => c + 1)
-    }
-
-    return (
-        <StyledButton {...props} variant="primary" onClick={onClick}>
-            {count}
-            Primary
-        </StyledButton>
-    )
+TemplateTest.args = {
+    variant: 'primary',
+    children: 'Primary',
 }
 
-export const Success = (props) => {
-    return (
-        <StyledButton {...props} variant="success">
-            Success
-        </StyledButton>
-    )
-}
 
-export const Transparent = (props) => {
-    return (
-        <StyledButton {...props} variant="transparent">
-            Transparent
-        </StyledButton>
-    )
-}
+// -----------------------------------------------
+
+// export default {
+//     title: 'StyledButton',
+//     component: StyledButton,
+// } as ComponentMeta<typeof StyledButton>
+
+// const incrementAction = action('increment')
+
+// export const Primary = (props) => {
+
+//     const [count, setCount] = useState(0)
+//     const onClick = (e: React.MouseEvent) => {
+//         incrementAction(e, count)
+//         setCount((c) => c + 1)
+//     }
+
+//     return (
+//         <StyledButton {...props} variant="primary" onClick={onClick}>
+//             {count}
+//             Primary
+//         </StyledButton>
+//     )
+// }
+
+// export const Success = (props) => {
+//     return (
+//         <StyledButton {...props} variant="success">
+//             Success
+//         </StyledButton>
+//     )
+// }
+
+// export const Transparent = (props) => {
+//     return (
+//         <StyledButton {...props} variant="transparent">
+//             Transparent
+//         </StyledButton>
+//     )
+// }
