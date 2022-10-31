7565ca3094b23e0ee9568ecd7866f5e5c14fbe2d
masakina0518 <masakina0518@gmail.com>
2022-10-31 16:53:56 +0900

    Jest追加＆設定

---
+++ app/jest.config.js
@@ -0,0 +1,11 @@
+const nextJext = require('next/jest')
+
+const createJestConfig = nextJext({ dir: './' })
+
+const customJestConfig = {
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testEnvironment: 'jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
