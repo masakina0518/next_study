4f8089b3af1ab4a9ef5d1eea5d975d7a9ce4c13c
masakina0518 <masakina0518@gmail.com>
2022-11-18 16:13:51 +0900

    storybook設定

---
+++ app/src/themes/index.ts
@@ -0,0 +1,13 @@
+import colors from './colors'
+import fontSizes from './fontSizes'
+import letterSpaceings from './fontSizes'
+import lineHeights from './lineHeights'
+import space from './space'
+
+export const theme = {
+    space,
+    fontSizes,
+    letterSpaceings,
+    lineHeights,
+    colors
+} as const
